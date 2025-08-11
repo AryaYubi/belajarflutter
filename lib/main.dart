fb76da009248a724ebb4f2e18d149fb7e3ee4b61
AryaYubi <aryayubi@icloud.com>
2025-08-11 17:00:45 +0700

    tes

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shamo/pages/sign_in_page.dart';
+import 'package:shamo/pages/splash_page.dart';
+import 'theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/splash': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+      },
+    );
+  }
+}
